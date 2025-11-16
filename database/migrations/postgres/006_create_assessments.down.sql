a019e7ec3b5d3508b8320653ab60aa5222eedf0e
Jhoan Medina <medinatello@gmail.com>
2025-11-15 22:33:47 -0300

    feat(database): agregar migraciones SQL completas
    - Crear 8 migraciones PostgreSQL (UP y DOWN)
      - 001: users
      - 002: schools
      - 003: academic_units
      - 004: memberships
      - 005: materials
      - 006: assessment
      - 007: assessment_attempt
      - 008: assessment_attempt_answer
    - Crear TABLE_OWNERSHIP.md documentando ownership
    - Todas las tablas con índices optimizados
    - Comentarios SQL en cada tabla

---
+++ database/migrations/postgres/006_create_assessments.down.sql
@@ -0,0 +1,5 @@
+DROP INDEX IF EXISTS idx_assessment_created_at;
+DROP INDEX IF EXISTS idx_assessment_status;
+DROP INDEX IF EXISTS idx_assessment_mongo;
+DROP INDEX IF EXISTS idx_assessment_material;
+DROP TABLE IF EXISTS assessment;
---
+++ database/migrations/postgres/008_create_assessment_answers.down.sql
@@ -0,0 +1,3 @@
+DROP INDEX IF EXISTS idx_answer_correct;
+DROP INDEX IF EXISTS idx_answer_attempt;
+DROP TABLE IF EXISTS assessment_attempt_answer;
